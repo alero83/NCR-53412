80096409b7b904e8fa80c5f48d8f590fae7d254d
BP <djoplin0703@gmail.com>
2021-10-14 00:19:49 +0000

    Primer comit

---
+++ prueba.py
@@ -0,0 +1,5 @@
+n1=input('Número One: ')
+n2=input('Número Two: ')
+
+suma=int(n1)+int(n2)
+print('La suma es: '+str(suma))
---
+++ prueba_js.js
@@ -0,0 +1,5 @@
+var n1=3
+var n2=4
+
+var suma=parseInt(n1)+parseInt(n2)
+console.log('La suma es: '+suma)
